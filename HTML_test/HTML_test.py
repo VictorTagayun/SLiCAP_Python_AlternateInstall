b6a2cbfe5daaa125e876b57cf8ef824f28e479b2
victagayun <vic.tagayun@yahoo.com>
2021-08-22 18:56:26 +0800

    Add files via upload

---
+++ HTML_test/HTML_test.py
@@ -0,0 +1,36 @@
+from SLiCAP import *
+
+prj = initProject('CS stage noise with resistive source')
+
+htmlPage('ASM OP-Amp Analysis',True,'label')
+
+file2html('OP-Amp-Analysis')
+ 
+# text2html('Not in any particular order')
+# text2html('Leong')
+# text2html('Yee Py')
+# text2html('Victor')
+# text2html('Das')
+# text2html('Baskaran')
+# text2html('Greggy')
+# text2html('Dinesh')
+
+# page1
+
+htmlPage('ASM OP-Amp Analysis1')
+
+file2html('OP-Amp-Analysis-y')
+
+
+# page2
+
+htmlPage('ASM OP-Amp Analysis2')
+
+file2html('OP-Amp-Analysis-y')
+
+
+# page3
+
+htmlPage('ASM OP-Amp Analysis3')
+
+file2html('OP-Amp-Analysis-y')
